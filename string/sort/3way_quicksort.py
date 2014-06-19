6b92a8e5ed9cae579ff583994f70fa63123743b4
LuoChunbo <luochunbo@gmail.com>
2014-06-19 10:03:37 +0800

    3way_quicksort.py: retab file
    Signed-off-by: LuoChunbo <luochunbo@gmail.com>

--- string/sort/3way_quicksort.py
+++ string/sort/3way_quicksort.py
@@ -1,45 +1,48 @@
 #/usr/bin/python
 
 def quick3string_sort(a):
-	sort(a, 0, len(a) - 1, 0)
+    sort(a, 0, len(a) - 1, 0)
 
 def swap(a, i, j):
-	tmp = a[i]
-	a[i] = a[j]
-	a[j] = tmp
+    tmp = a[i]
+    a[i] = a[j]
+    a[j] = tmp
 
 def sort(a, lo, hi ,d):
-	if lo >= hi:
-		return
+    if lo >= hi:
+        return
 
-	left = lo
-	right = hi
+    left = lo
+    right = hi
 
-	if len(a[lo]) <= d:
-		return
-	
-	v = a[lo][d]
-	i = lo + 1
+    if len(a[lo]) <= d:
+        v = -1
+    else:
+        v = a[lo][d]
 
-	while i <= right:
-		if len(a[i]) <= d:
-			t = -1
-		else:
-			t = a[i][d]
+    
+    i = lo + 1
 
-		if t < v:
-			swap(a, left, i)
-			left += 1
-			i += 1
-		elif t > v:
-			swap(a, right, i)
-			right -= 1
-		else:
-			i += 1
-	
-	sort(a, lo, left-1, d)
-	sort(a, left, right, d+1)
-	sort(a, right+1, hi, d)
+    while i <= right:
+        if len(a[i]) <= d:
+            t = -1
+        else:
+            t = a[i][d]
+
+        if t < v:
+            swap(a, left, i)
+            left += 1
+            i += 1
+        elif t > v:
+            swap(a, right, i)
+            right -= 1
+        else:
+            i += 1
+    
+    sort(a, lo, left-1, d)
+    if v >= 0:
+        sort(a, left, right, d+1)
+    sort(a, right+1, hi, d)
 
 
 
