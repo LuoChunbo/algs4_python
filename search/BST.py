c103921406a57117e7afcd6331c89411e575ec16
Luo Chunbo <luochunbo@gmail.com>
2014-07-01 23:59:54 +0800

    BST.py: implement BST as a class
    Signed-off-by: Luo Chunbo <luochunbo@gmail.com>

--- search/BST.py
+++ search/BST.py
@@ -10,228 +10,246 @@ class Node:
         self.right = None;
 
 
-root = None;
 
-"""def size():
-        return size(root);
-"""
-def size(x):
-    if not x:
-        return 0;
+class BST:
 
-    return x.N;
+    #root = None;
 
-"""def get(key):
-    return get(root, key);
-"""
-def get(x, key):
-    if not x:
-        return None;
+    def __init__(self):
+        self.root = None;
+        self.node_max = Node("",0, 0);
 
-    if key < x.key:
-        return get(x.left, key);
-    elif key > x.key:
-        return get(x.right, key);
-    else:
-        return x.val;
+    def node_size(self, x):
+        if not x:
+            return 0;
+        return x.N;
 
+    def size(self):
+        return self.node_size(self.root);
 
-"""def put(key, val):
-    root = put(root, key, val)
-"""
-def put(x, key, val):
-    if not x:
-        return Node(key, val, 1);
+    def insert(self, x, key):
+        if not x:
+            return Node(key, 1, 1);
+        
+        if key < x.key:
+            x.left = self.insert(x.left, key);
+        elif key > x.key:
+            x.right = self.insert(x.right, key);
+        else:
+            x.val += 1;
 
-    if key < x.key:
-        x.left = put(x.left, key, val);
-    elif key > x.key:
-        x.right = put(x.right, key, val);
-    else:
-        x.val = val;
+        x.N = self.node_size(x.left) + self.node_size(x.right) + 1;
+
+        return x;
+
+    def put(self, key):
+        self.root = self.insert(self.root, key);
+
+    def search(self, x, key):
+        if not x:
+            return None;
+
+        if key < x.key:
+            return self.get(x.left, key);
+        elif key > x.key:
+            return self.get(x.right, key);
+        else:
+            return x.val;
+
+    def get(self, key):
+        return self.search(self.root, key);
+
+    def inorder_walk(self, x):
+        if not x:
+            return;
+        self.inorder_walk(x.left);
+        print("key =", x.key, "val=", x.val);
+        self.inorder_walk(x.right);
+
+    def traverse_tree(self):
+        return self.inorder_walk(self.root);
+
+    def get_node_max(self):
+        return self.max_val(self.root);
+
+    def max_val(self, x):
+        if not x:
+            return None;
+        
+        self.max_val(x.left);
+
+        if x.val > self.node_max.val:
+            self.node_max = x;
+
+        self.max_val(x.right);
+
+        return self.node_max;
+
+    def node_min(self, x):
+        if not x.left:
+            return x;
+        else:
+            return self.node_min(x.left);
+
+    def min(self):
+        return self.node_min(self.root).key;
+
+    def node_max(self, x):
+        if not x.right:
+            return x;
+        else:
+            return self.node_max(x.right);
+
+    def max(self):
+        return self.node_max(self.root).key;
+
     
-    x.N = size(x.left) + size(x.right) + 1;
-
-    return x;
-
-
-def min():
-    return min(root).key;
-
-def min(x):
-    if not x.left:
-        return x;
-    else:
-        return min(x.left);
-
-def max():
-    return max(root).key;
-
-def max(x):
-    if not x.right:
-        return x;
-    else:
-        return max(x.right);
-
-
-def floor(key):
-    x = floor(root, key);
-    if not x:
-        return None;
-    return x.key;
-
-def floor(x, key):
-    if not x:
-        return None;
-
-    if x.key == key:
-        return x;
-
-    elif key < x.key:
-        return floor(x.left, key);
-    else:
-        t = floor(x.right, key);
-        if not t:
+    def node_floor(self, x, key):
+        if not x:
+            return None;
+        
+        if key == x.key:
             return x;
+
+        if key < x.key:
+            return self.node_floor(x.left, key);
         else:
-            return t;
+            t = self.node_floor(x.right, key);
+            if not t:
+                return x;
+            else:
+                return t;
 
-def ceiling(key):
-    x = ceiling(root, key);
-    if not x:
-        return None;
-    return x.key
+    def floor(self, key):
+        x = self.node_floor(self.root, key);
+        if not x:
+            return None;
 
-def ceiling(x, key):
-    if not x:
-        return None;
+        return x.key;
 
-    if key == x.key:
-        return x;
+    def node_ceiling(self, x, key):
+        if not x:
+            return None;
 
-    elif key > x.key:
-        return ceiling(x.right, key)
-    else:
-        t = ceiling(x.left, key);
-        if not t:
+        if key == x.key:
             return x;
+
+        if key > x.key:
+            return self.node_ceiling(x.right, key);
         else:
-            return t;
+            t = self.node_ceiling(x.left, key);
+            if not t:
+                return x;
+            else:
+                return t;
 
 
-def select(k):
-    return select(root, k).key;
+    def ceiling(self, key):
+        x = self.node_ceiling(self.root, key);
+        if not x:
+            return None;
 
-def select(x, k):
-    if not x:
-        return None;
+        return x.key;
 
-    t = size(x.left);
-    if t < k:
-        return select(x.right, k - t - 1);
-    elif t > k:
-        return select(x.left, k);
-    else:
-        return x;
+    def node_select(self, x, k):
+        if not x:
+            return None;
 
-def rank(key):
-    return rank(root, key);
+        t = self.node_size(x.left);
 
-def rank(x, key):
-    if not x:
-        return 0;
-
-    if key < x.key:
-        return rank(x.left, key);
-    elif key > x.key:
-        return  1 + size(x.left) + rank(x.right, key);
-    else:
-        return size(x.left);
-
-
-def deleteMin():
-    root = deleteMin(root);
-
-
-def deleteMin(x):
-    if not x.left:
-        return x.right;
-
-    x.left = deleteMin(x.left);
-    x.N = size(x.left) + size(x.right) + 1;
-    return x;
-
-def deleteMax(x):
-    root = deleteMax(root);
-
-def deleteMax(x):
-    if not x.right:
-        return x.left;
-
-    x.right = deletcMax(x.right);
-    x.N = size(x.left) + size(x.right) + 1;
-    return x;
-
-def delete(key):
-    root = delete(root, key);
-
-def delete(x, key):
-    if not x:
-        return None;
-
-    if key < x.key:
-        x.left = delete(x.left, key);
-    elif key > x.key:
-        x.right = delecte(x.right, key);
-    else:
-        if x.right == None:
+        if t == k:
             return x.left;
-        if x.left == None:
+        elif t > k:
+            return self.node_select(x.left, k);
+        else:
+            return self.node_select(x.right, k - t -1);
+
+    def select(self, k):
+        x = self.node_select(self.root, k);
+        if not x:
+            return None;
+        return x.key;
+
+
+    def node_rank(self, x, key):
+        if not x:
+            return 0;
+
+        if key < x.key:
+            return self.node_rank(x.left, key);
+        elif key > x.key:
+            return 1 + self.node_size(x.left) + self.node_rank(x.right, key);
+        else:
+            return node_size(x.left);
+
+    def rank(self, key):
+        return self.node_rank(self.root, key);
+
+    
+    def node_deleteMin(self, x):
+        if not x.left:
             return x.right;
 
-        t = x;
-        x = min(t.right);
-        x.right = deleteMin(t.right);
-        x.left = t.left;
+        x.left = self.node_deleteMin(x.left);
+        x.N = self.node_size(x.left) + self.node_size(x.right) + 1;
 
-    x. N = size(x.left) + size(x.right) + 1;
+        return x;
 
-    return x;
+    def deleteMin(self):
+        self.root = self.node_deleteMin(self.root);
 
-def contains(key):
-    return get(root, key) != None
 
-def iterator_node(x):
-    if not x:
-        return;
-    iterator_node(x.left);
-    print("key = ", x.key , "value = ", x.val);
-    iterator_node(x.right)
+    def node_deleteMax(self, x):
+        if not x.right:
+            return x.left;
 
-max_key=""
-max_val=0
+        x.right = self.node_deleteMax(x.right);
+        x.N = self.node_size(x.left) + self.node_size(x.right) + 1;
+        return x;
 
-def find_max_node(x):
-    if not x:
-        return;
+    def deleteMax(self):
+        self.root = node_deleteMax(self.root);
 
-    global max_key;
-    global max_val;
 
-    find_max_node(x.left);
-    find_max_node(x.right);
-    if x.val > max_val:
-        max_val = x.val;
-        max_key = x.key;
+    def node_delete(self, x, key):
+        if not x:
+            return None;
+
+        if key < x.key:
+            return self.node_delete(x.left, key);
+
+        elif key > x.key:
+            return self.node_delete(x.right, key);
+        else:
+            if x.right == None:
+                return x.left;
+            if x.left == None:
+                return x.right;
+
+            t = x;
+            x = self.node_min(t.right);
+            x.right = self.node_deleteMin(t.right);
+            x.left = t.left;
+
+        x.N = self.node_size(x.left) + self.node_size(x.right) + 1;
+
+        return x;
+
+    def delete(self, key):
+        self.root = self.node_delete(self.root, key);
+
+
 
 
 if __name__ == "__main__":
 
-    minlen = 8;
+    minlen = 10;
     global root;
-    #f = open("tinyTale.txt");
+    f = open("tinyTale.txt");
     #f = open("tale.txt");
-    f = open("leipzig1M.txt");
+    #f = open("leipzig1M.txt");
+
+    st = BST();
 
     words = 0;
     for line in f:
@@ -239,22 +257,20 @@ if __name__ == "__main__":
             words += len(word);
             if len(word) < minlen:
                 continue;
-            #print(word)
-            if not contains(word):
-                root = put(root, word, 1);
-            else:
-                root = put(root, word, get(root, word) + 1);
+
+            st.put(word);
+
     f.close()
 
-    #iterator_node(root)
+    #st.traverse_tree();
 
-    find_max_node(root)
+    node = st.get_node_max();
 
-    print("all words = ", words);
+    print("max node key = " + str(node.key) + " ,val = " + str(node.val));
 
-    print("max node key: ", max_key, "max node val :", max_val);
+    print("word size :", st.root.N);
 
-    print("word size: ", root.N);
+
 
 
 
